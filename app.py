e595d8168d27990e79b3f67eb1894c39ba1015ec
LiseRochat <liserochat@live.fr>
2022-05-14 10:29:10 +0200

    Corect parse error: to add movie in json file use w mode not a

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from PySide2 import QtWidgets 
+
+from movie import get_movies
+from PySide2 import QtWidgets, QtCore
 
 class App(QtWidgets.QWidget):
     def __init__(self):
@@ -12,14 +14,22 @@ class App(QtWidgets.QWidget):
         """
         self.layout = QtWidgets.QVBoxLayout(self)
         self.line_movie = QtWidgets.QLineEdit()
-        self.btn_add_moovie = QtWidgets.QPushButton("Ajouter un film")
-        self.list_moovie = QtWidgets.QListWidget()
-        self.btn_delete_moovie = QtWidgets.QPushButton("Supprimer le(s) film(s)")
+        self.btn_add_movie = QtWidgets.QPushButton("Ajouter un film")
+        self.list_movie = QtWidgets.QListWidget()
+        self.btn_delete_movie = QtWidgets.QPushButton("Supprimer le(s) film(s)")
 
         self.layout.addWidget(self.line_movie)
-        self.layout.addWidget(self.btn_add_moovie)
-        self.layout.addWidget(self.list_moovie)
-        self.layout.addWidget(self.btn_delete_moovie)
+        self.layout.addWidget(self.btn_add_movie)
+        self.layout.addWidget(self.list_movie)
+        self.layout.addWidget(self.btn_delete_movie)
+    
+    def populate_movies(self):
+        movies = get_movies()
+
+        for movie in movies:
+            lw_item = QtWidgets.QListWidgetItem(movie.title)
+            lw_item.setData(QtCore.Qt.UserRole, movie)
+            self.list_movie.addItem(lw_item)
 
 app = QtWidgets.QApplication([])
 win = App()
